739ef1eef19d4db14f68c768756208954756b3e8
Dollarman36 <dollarmannnadi.com>
2022-12-09 16:27:27 -0800

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
-
 def square_matrix_simple(matrix=[]):
-    new = [row[:] for row in matrix]
-    for idx, row in enumerate(new):
-        for idx2, col in enumerate(row):
-            new[idx][idx2] = row[idx2] ** 2
-            return new
+    tmp = []
+    for x in matrix:
+        tmp.append(list(map(lambda x: x**2, x)))
+        return (tmp)
