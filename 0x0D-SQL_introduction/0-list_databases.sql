ed7e0f1b1d21f877a3a96c0d644667d5877dee2d
Dollarman36 <111034852+Dollarman36@users.noreply.github.com>
2023-02-15 01:46:07 +0100

    Add files via upload

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,2 @@
+-- 0-list_databases  lists all databases of your MySQL server.
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- 3-list_tables lists all the tables of a database in your MySQL server.
+SHOW tables;
